4ccf53527dc4c71d990ea2c6463435509d7d775b
Raymundo <rrosalesr1500@alumno.ipn.mx>
2021-12-03 11:23:38 -0600

    se agregan archivos necesarios para creacion de contenedores correspondientes al servidor y base de datos

---
+++ practica/sql_scripts/crear_tabla.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS TestDB;
+
+use TestDB;
+
+CREATE TABLE IF NOT EXISTS PERSON(
+  id INTEGER primary key not null auto_increment,
+  name VARCHAR(100) not null,
+  country VARCHAR(100) not null 
+);
